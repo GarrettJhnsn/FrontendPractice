4fb48035246567596f3f5e8f1c7cefe75f6773b2
Garrett M Johnson <45772450+GarrettJhnsn@users.noreply.github.com>
2023-04-05 11:16:26 -0700

    Expanding Panels
    Expanding Panel Project Added

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,1 +1,18 @@
-const x = 'test'
+const panels = document.querySelectorAll('.panel')
+
+
+//Adds Active Class To Selected Panel
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+//Function To Remove Active From Non Active Classess
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
