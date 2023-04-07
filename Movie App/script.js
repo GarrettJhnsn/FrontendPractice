4e422c6da2ccbd61cfe6e1a05c1e84087cfb44b3
Garrett M Johnson <45772450+GarrettJhnsn@users.noreply.github.com>
2023-04-07 12:22:15 -0700

    Testimonial
    Testimonial

--- Movie App/script.js
+++ Movie App/script.js
@@ -1,5 +1,5 @@
 // TMDB API CONNECTIONS
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=204e5146125d6ae3c9061daf9136d1c8&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=204e5146125d6ae3c9061daf9136d1c8&query="'
 
